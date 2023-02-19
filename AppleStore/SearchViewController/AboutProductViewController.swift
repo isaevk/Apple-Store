076621e8ad2d565a0938a5ad93b17ce1a0924fe6
isaevk <kirill_isaev@aol.com>
2023-02-19 22:00:12 +0600

    bug fix

--- AppleStore/SearchViewController/AboutProductViewController.swift
+++ AppleStore/SearchViewController/AboutProductViewController.swift
@@ -11,15 +11,14 @@ import WebKit
 final class AboutProductViewController: UIViewController {
   
   // MARK: - Public Properties
-  var myURL: String?
+  var myURL = String()
   
   // MARK: - Private Properties
   private let webView = WKWebView()
   private let goBackItem = UIBarButtonItem(systemItem: .rewind)
   private let goForwardItem = UIBarButtonItem(systemItem: .fastForward)
   private let resetItem = UIBarButtonItem(systemItem: .refresh)
-  private let activityIndicator = UIActivityIndicatorView()
-  private let aplication = UIApplication.shared
+  private let networkIndicatorView = UIActivityIndicatorView()
   
   // MARK: - Override Methods
   override func viewDidLoad() {
@@ -45,18 +44,29 @@ final class AboutProductViewController: UIViewController {
   
   // MARK: - Private methods
   private func setupConsteaints() {
-    webView.translatesAutoresizingMaskIntoConstraints = false
+    [webView, networkIndicatorView].forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+    }
     view.addSubview(webView)
+    webView.addSubview(networkIndicatorView)
     
     NSLayoutConstraint.activate([
       webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
       webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
-      webView.widthAnchor.constraint(equalToConstant: view.frame.width)
+      webView.widthAnchor.constraint(equalToConstant: view.frame.width),
+      networkIndicatorView.centerXAnchor.constraint(equalTo: webView.centerXAnchor),
+      networkIndicatorView.centerYAnchor.constraint(equalTo: webView.centerYAnchor)
     ])
   }
   
   private func setupUI() {
-    guard let url = URL(string: myURL!) else { return }
+    guard let url = URL(string: myURL) else {
+      if let urlError = URL(string: "https://tangycard.s3-tastewp.com/404page-test-a30f6d66eb61e947c49291d36aab8953") {
+        let myRequestError = URLRequest(url: urlError)
+        webView.load(myRequestError)
+      }
+      return
+    }
     let myRequest = URLRequest(url: url)
     webView.load(myRequest)
     
@@ -70,12 +80,12 @@ final class AboutProductViewController: UIViewController {
 
 // MARK: - Extension WKNavigationDelegate
 extension AboutProductViewController: WKNavigationDelegate {
-  func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-    aplication.isNetworkActivityIndicatorVisible = true
+  func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation) {
+    networkIndicatorView.startAnimating()
   }
   
-  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-    aplication.isNetworkActivityIndicatorVisible = false
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation) {
+    networkIndicatorView.stopAnimating()
     goBackItem.isEnabled = webView.canGoBack
     goForwardItem.isEnabled = webView.canGoForward
   }
